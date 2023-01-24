748f0aa31d9911afd3b60680cd84e5849a630100
Maratmost <maratmost89@gmail.com>
2023-01-24 21:08:31 +0300

    Initial Commit

---
+++ Lesson 2.2. UIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.2. UIKitApp
+//
+//  Created by maratmost on 24/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
