f10c4e4f4be2cddd844d1c8e8f7e1be8f0d9ed74
Maratmost <maratmost89@gmail.com>
2023-01-25 13:22:24 +0300

    Add second

--- Lesson 2.2. UIKitApp/ViewController.swift
+++ Lesson 2.2. UIKitApp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // MARK: Segmented Control
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        // MARK: LAbel
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        // MARK: Slider
+        
     }
 
-
+    @IBAction func segmentedControlAction(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "Выбран сегмент с индексом 0"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "выбран сегмент с индексом 1"
+            mainLabel.textColor = .yellow
+        case 2:
+            mainLabel.text = "выбран сегмент с индексом 2"
+            mainLabel.textColor = .green
+        default: break
+        }
+    }
 }
 
