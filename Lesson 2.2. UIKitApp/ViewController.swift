7afa6bb92cf599e02fc91c772b133401d13afe84
Maratmost <maratmost89@gmail.com>
2023-01-25 18:09:40 +0300

    New 2app

--- Lesson 2.2. UIKitApp/ViewController.swift
+++ Lesson 2.2. UIKitApp/ViewController.swift
@@ -10,21 +10,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var segmentedControl: UISegmentedControl!
-    
     @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var mainButton: UIButton!
+    @IBOutlet var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // MARK: Segmented Control
         segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
         
-        // MARK: LAbel
+        // MARK: Label
         mainLabel.text = ""
         mainLabel.font = mainLabel.font.withSize(30)
         mainLabel.textAlignment = .center
         mainLabel.numberOfLines = 2
         
         // MARK: Slider
+        slider.value = 1
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .blue
+        slider.thumbTintColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
+        
+        mainLabel.text = String(slider.value)
+        
+        // MARK: TextField
+        textField.backgroundColor = .white
+        textField.keyboardType = .numberPad
+        
+        // MARK: Button
+        mainButton.layer.cornerRadius = 10
+        mainButton.setTitle("Готово", for: .normal)
+        
+        // MARK: datePicker
         
     }
 
@@ -41,6 +62,22 @@ class ViewController: UIViewController {
             mainLabel.textColor = .green
         default: break
         }
+    }
+    
+    @IBAction func sliderAction() {
+        mainLabel.text = String(Int(slider.value))
+    }
+    
+    @IBAction func mailButtonAction(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else { return }
+        mainLabel.text = text
+    }
+
+    @IBAction func datePickerAction(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        
+        mainLabel.text = dateFormatter.string(from: datePicker)
     }
 }
 
